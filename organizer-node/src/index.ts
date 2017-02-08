14d395099e955b246c2a01b59061f56d4067b7e6
Nathaniel Rook <nrook@nrook.com>
2017-02-07 18:42:07 -0800

    Slightly extend stadium_service.ts.

--- organizer-node/src/index.ts
+++ organizer-node/src/index.ts
@@ -7,12 +7,12 @@ import { startServer } from './stadium_service';
 import * as logger from 'winston';
 logger.cli();
 
-startServer();
+const LOCALHOST_URL = 'ws://localhost:8000/showdown/websocket';
 
-// const LOCALHOST_URL = 'ws://localhost:8000/showdown/websocket';
+const teamClient = new TeamClient(8080);
+const battleClient = new BattleClient(8080);
 
-// const teamClient = new TeamClient(8080);
-// const battleClient = new BattleClient(8080);
+startServer(LOCALHOST_URL, teamClient, battleClient);
 
 // connect(LOCALHOST_URL).then((connection) => {
 //   return new ShowdownDirector(connection, teamClient, battleClient);
--- organizer-node/src/stadium_service.ts
+++ organizer-node/src/stadium_service.ts
@@ -6,15 +6,23 @@ import * as grpc from 'grpc';
 
 import * as logger from 'winston';
 
-import {stadiumFile} from './proto_constants';
+import { BattleClient } from './ai_client';
+import { TeamClient } from './teamclient';
+import { stadiumFile } from './proto_constants';
+
 const stadiumDescriptor = grpc.load(stadiumFile);
 
 const PORT = 8081;
 
-export function startServer(): void {
+export function startServer(
+    showdownWebSocketUrl: String,
+    teamClient: TeamClient,
+    battleClient: BattleClient): void {
+  const stadiumServer: StadiumServer = new StadiumServer(showdownWebSocketUrl, teamClient, battleClient);
+
   const server = new grpc.Server();
   server.addProtoService(stadiumDescriptor.kadabra.StadiumService.service, {
-    selfPlay: handleSelfPlay
+    selfPlay: stadiumServer.handleSelfPlay.bind(stadiumServer)
   });
   const port = server.bind(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure());
   if (port !== 8081) {
@@ -24,6 +32,21 @@ export function startServer(): void {
   server.start();
 }
 
-function handleSelfPlay(selfPlayRequest): {} {
-  throw Error('no');
+class StadiumServer {
+  private showdownWebSocketUrl: String;
+  private teamClient: TeamClient;
+  private battleClient: BattleClient;
+
+  constructor(
+      showdownWebSocketUrl: String,
+      teamClient: TeamClient,
+      battleClient: BattleClient) {
+    this.showdownWebSocketUrl = showdownWebSocketUrl;
+    this.teamClient = teamClient;
+    this.battleClient = battleClient;
+  }
+
+  handleSelfPlay(selfPlayRequest): {} {
+    throw Error('no');
+  }
 }
