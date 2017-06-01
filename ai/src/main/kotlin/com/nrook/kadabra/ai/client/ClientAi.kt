d2127dee69d028b67ff4c5fe43b753fbb689471d
Nathaniel Rook <nrook@nrook.com>
2017-06-01 10:33:32 -0700

    ClientAI cleanup

--- ai/src/main/kotlin/com/nrook/kadabra/ai/client/ClientAi.kt
+++ ai/src/main/kotlin/com/nrook/kadabra/ai/client/ClientAi.kt
@@ -1,5 +1,11 @@
 package com.nrook.kadabra.ai.client
 
+import com.nrook.kadabra.proto.ActionRequest
+import com.nrook.kadabra.proto.ActionResponse
+import com.nrook.kadabra.proto.LeadChoice
+import com.nrook.kadabra.proto.MoveSelection
+import com.nrook.kadabra.proto.SwitchSelection
+
 
 private val random: java.util.Random = java.util.Random()
 private val logger = mu.KLogging().logger()
@@ -8,13 +14,13 @@ private val logger = mu.KLogging().logger()
  * Makes decisions. Will probably be an interface someday.
  */
 class ClientAi {
-  fun pickLead(): com.nrook.kadabra.proto.LeadChoice {
-    return com.nrook.kadabra.proto.LeadChoice.newBuilder()
-        .setLeadIndex(com.nrook.kadabra.ai.client.random.nextInt(6) + 1)
+  fun pickLead(): LeadChoice {
+    return LeadChoice.newBuilder()
+        .setLeadIndex(random.nextInt(6) + 1)
         .build()
   }
 
-  fun pickAction(request: com.nrook.kadabra.proto.ActionRequest): com.nrook.kadabra.proto.ActionResponse {
+  fun pickAction(request: ActionRequest): ActionResponse {
     if (request.forceSwitch) {
       val team = request.sideInfo.teamList
 
@@ -32,21 +38,19 @@ class ClientAi {
         throw IllegalArgumentException("All these Pokemon are fainted! What's going on?")
       }
 
-      return com.nrook.kadabra.proto.ActionResponse.newBuilder()
-          .setSwitch(com.nrook.kadabra.proto.SwitchSelection.newBuilder()
-              .setIndex(indices[com.nrook.kadabra.ai.client.random.nextInt(indices.size)]))
+      return ActionResponse.newBuilder()
+          .setSwitch(SwitchSelection.newBuilder()
+              .setIndex(indices[random.nextInt(indices.size)]))
           .build()
     } else {
       val legalMoves: List<Int> = (0 until request.moveCount)
           .filterNot { request.moveList[it].disabled }
           .toList()
 
-      val moveIndex = legalMoves[com.nrook.kadabra.ai.client.random.nextInt(legalMoves.size)] + 1
+      val moveIndex = legalMoves[random.nextInt(legalMoves.size)] + 1
 
-      return com.nrook.kadabra.proto.ActionResponse.newBuilder()
-          .setMove(
-              com.nrook.kadabra.proto.MoveSelection.newBuilder()
-                  .setIndex(moveIndex))
+      return ActionResponse.newBuilder()
+          .setMove(MoveSelection.newBuilder().setIndex(moveIndex))
           .build()
     }
   }
