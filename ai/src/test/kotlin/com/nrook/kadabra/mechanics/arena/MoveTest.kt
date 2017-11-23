647098fb6e3f77af18dbd14f211167208158e864
Nathaniel Rook <nrook@nrook.com>
2017-11-23 12:33:39 -0800

    Finish u-turn implementation.

--- ai/src/test/kotlin/com/nrook/kadabra/mechanics/arena/MoveTest.kt
+++ ai/src/test/kotlin/com/nrook/kadabra/mechanics/arena/MoveTest.kt
@@ -26,8 +26,6 @@ class MoveTest {
 
   @Test
   fun uTurnActsFirst() {
-    // Not yet!
-    /*
     val scizor = TestSpecBuilder.create(pokedex, "Scizor")
         .withMoves("uturn")
         .build()
@@ -35,7 +33,7 @@ class MoveTest {
         .withMoves("return")
         .build()
     val snorlax = TestSpecBuilder.create(pokedex, "Snorlax")
-        .withMoves("return")
+        .withMoves("facade")
         .build()
 
     val battle = startBattle(listOf(scizor, tauros), 0, listOf(snorlax), 0, context)
@@ -43,18 +41,51 @@ class MoveTest {
         battle,
         context,
         MoveChoice(pokedex.getMoveByName("U-turn")),
-        MoveChoice(pokedex.getMoveByName("Return")))
+        MoveChoice(pokedex.getMoveByName("Facade")))
 
-    // TODO: actually check damage numbers
+    assertThat(afterFirstUTurn.phase).isEqualTo(Phase.FIRST_MOVE_SWITCH)
     assertThat(afterFirstUTurn.blackSide.active.species.name).isEqualTo("Scizor")
     assertThat(afterFirstUTurn.choices(Player.BLACK))
         .containsExactly(SwitchChoice(tauros.species.id))
-    assertThat(afterFirstUTurn.whiteSide.active.hp).isEqualTo(316)  // Totally made up...
+    assertThat(afterFirstUTurn.whiteSide.active.hp).isEqualTo(316)
     assertThat(afterFirstUTurn.choices(Player.WHITE)).isEmpty()
 
-    val endOfTurn = simulateBattle(battle, context, SwitchChoice(tauros.species.id), null)
+    val endOfTurn = simulateBattle(
+        afterFirstUTurn, context, SwitchChoice(tauros.species.id), null)
     assertThat(endOfTurn.blackSide.active.species.name).isEqualTo("Tauros")
-    assertThat(endOfTurn.blackSide.active.hp).isEqualTo(200)  // Still made up
-    */
+    assertThat(endOfTurn.blackSide.active.hp).isEqualTo(198)
+  }
+
+  @Test
+  fun uTurnActsSecond() {
+    val scizor = TestSpecBuilder.create(pokedex, "Scizor")
+        .withMoves("uturn")
+        .build()
+    val tauros = TestSpecBuilder.create(pokedex, "Tauros")
+        .withMoves("return")
+        .build()
+    val jolteon = TestSpecBuilder.create(pokedex, "Jolteon")
+        .withMoves("thunderbolt")
+        .build()
+
+    val battle = startBattle(listOf(scizor, tauros), 0, listOf(jolteon), 0, context)
+    val uTurnSwitch = simulateBattle(
+        battle,
+        context,
+        MoveChoice(pokedex.getMoveByName("U-turn")),
+        MoveChoice(pokedex.getMoveByName("Thunderbolt")))
+
+    assertThat(uTurnSwitch.phase).isEqualTo(Phase.SECOND_MOVE_SWITCH)
+    assertThat(uTurnSwitch.blackSide.active.species.name).isEqualTo("Scizor")
+    assertThat(uTurnSwitch.choices(Player.BLACK))
+        .containsExactly(SwitchChoice(tauros.species.id))
+    assertThat(uTurnSwitch.blackSide.active.hp).isEqualTo(143)  // 138 damage
+    assertThat(uTurnSwitch.whiteSide.active.hp).isEqualTo(117)  // 154 damage
+    assertThat(uTurnSwitch.choices(Player.WHITE)).isEmpty()
+
+    val endOfTurn = simulateBattle(
+        uTurnSwitch, context, SwitchChoice(tauros.species.id), null)
+    assertThat(endOfTurn.blackSide.active.species.name).isEqualTo("Tauros")
+    assertThat(endOfTurn.blackSide.active.hp).isEqualTo(291)  // full
   }
 }
